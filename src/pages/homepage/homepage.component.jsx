6b9feb1dc9e6d6b69314f6aad125535e1f53ad7b
Ozan Deniz <ozanddemirtas@gmail.com>
2020-05-08 14:06:40 +0300

    folders-created

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./homepage.styles.scss";
 
 const HomePage = () => (
   <div className="homepage">
@@ -8,7 +9,7 @@ const HomePage = () => (
           <h1 className="title">HATS</h1>
           <span className="subtitle">SHOP NOW</span>
         </div>
-      </div>{" "}
+      </div>
       <div className="menu-item">
         <div className="content">
           <h1 className="title">JACKETS</h1>
