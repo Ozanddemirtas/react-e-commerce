adb67edc43e70db03eca0aebd2e973c6f12be67b
Ozan Deniz <ozanddemirtas@gmail.com>
2020-06-04 12:23:46 +0300

    cart-icon-dropdown-hidden/fuctionalty-added

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,14 +1,21 @@
 import React from "react";
+import { connect } from "react-redux";
 
-import "./cart-icon.styles.scss";
+import { toggleCartHidden } from "../../redux/cart/cart.actions";
 
 import { ReactComponent as ShoppingIcon } from "../../assets/shoppingIcon.svg";
 
-const CartIcon = () => (
-  <div className="cart-icon">
+import "./cart-icon.styles.scss";
+
+const CartIcon = ({ toggleCartHidden }) => (
+  <div className="cart-icon" onClick={toggleCartHidden}>
     <ShoppingIcon className="shopping-icon" />
     <span className="item-count">0</span>
   </div>
 );
 
-export default CartIcon;
+const mapDispatchToProps = (dispatch) => ({
+  toggleCartHidden: () => dispatch(toggleCartHidden()),
+});
+
+export default connect(null, mapDispatchToProps)(CartIcon);
